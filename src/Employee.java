428e97b03211aa05763d66e89f88fa98f69c223f
MARKTMM <MARKMWANGI304@GMAIL.COM>
2024-04-11 00:43:26 +0300

    class represents an employee with common attributes like id and name,along with additional attributes related to salary

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,34 @@
-public class Employee {
+/**
+ * This class represents an employee with common attributes like id and name,
+ * along with additional attributes related to salary.
+ */
+public class Employee extends Person {
+    private int paidSalary;
+
+    /**
+     * Constructs an Employee object with the specified id, name, and paid salary.
+     * @param id The unique identifier of the employee.
+     * @param name The name of the employee.
+     * @param paidSalary The amount of salary paid to the employee.
+     */
+    public Employee(int id, String name, int paidSalary) {
+        super(id, name); // Call the constructor of the superclass (Person)
+        this.paidSalary = paidSalary;
+    }
+
+    /**
+     * Retrieves the amount of salary paid to the employee.
+     * @return The amount of salary paid.
+     */
+    public int getPaidSalary() {
+        return paidSalary;
+    }
+
+    /**
+     * Adds the specified amount to the paid salary of the employee.
+     * @param amount The amount of salary to be added.
+     */
+    public void addPaidSalary(int amount) {
+        paidSalary += amount;
+    }
 }
