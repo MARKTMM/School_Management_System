56a8407c7577bff8ce799db0dc3c33fb7f6e48ed
MARKTMM <MARKMWANGI304@GMAIL.COM>
2024-04-14 19:42:28 +0300

    Added courses to Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,8 @@ public class Main {
     public static void main(String[] args) {
         // Create instances of Teacher class
         Teacher victor = new Teacher(1, "Victor Chinedum", 0, "Devops Java", 200000);
-        Teacher kunal = new Teacher(2, "Kunal Kushwaha", 0, "Unknown", 150000); // Corrected name
-        Teacher cyril = new Teacher(3, "Cyril Tumbo", 0, "Unknown", 150000); // Corrected name
+        Teacher kunal = new Teacher(2, "Kunal Kushwaha", 0, "Unknown", 150000);
+        Teacher cyril = new Teacher(3, "Cyril Tumbo", 0, "Unknown", 150000);
 
         // Create a list to store teachers
         List<Teacher> teacherList = new ArrayList<>();
@@ -21,15 +21,34 @@ public class Main {
         teacherList.add(kunal);
         teacherList.add(cyril);
 
+        /** Create a list to store coursesTaught
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(Devops_java);
+         */
+
         // Create instances of Student class
-        Student mark = new Student(1, "Mark Mwangi", "Devops Java", "Cohort 15", 0, 100000);
+        Student mark = new Student(1, "Mark Mwangi", "DevopsJava", "Cohort 15", 0, 100000);
         Student caleb = new Student(2, "Caleb Baraka", "Fullstack Engineer", "Cohort 19", 0, 80000);
-        Student jennifer = new Student(3, "Jennifer Karanja", "Cyber Security", "Cohort 15", 0 , 90000); // Corrected name
+        Student jennifer = new Student(3, "Jennifer Karanja", "Cyber Security", "Cohort 15", 0 , 90000);
 
         // Create a list to store students
         List<Student> studentList = new ArrayList<>();
         studentList.add(mark);
         studentList.add(caleb);
         studentList.add(jennifer);
+
+        // Create instances of courses class
+        Course devops = new Course("Devops Java", 001);
+        Course fullstack = new Course("Fullstack Engineer", 002);
+        Course security = new Course("Cyber Security", 003);
+
+        // Create a list to store courses
+        List<Course> coursesList = new ArrayList<>();
+        coursesList.add(devops);
+        coursesList.add(fullstack);
+        coursesList.add(security);
+
+        School markUniversity = new School(teacherList, studentList, coursesList);
+        System.out.println("Mark University has earned " + markUniversity.getTotalMoneyEarned());
     }
 }
