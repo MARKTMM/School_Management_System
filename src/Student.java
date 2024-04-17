5129209d590e702059fdcd344b365544439cf678
MARKTMM <MARKMWANGI304@GMAIL.COM>
2024-04-18 00:11:56 +0300

    Removed List feature in Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,11 @@
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * This class represents a student at the school.
  */
 public class Student extends Person {
-    private List<Course> courses;
-    private List<Cohort> cohorts;
+    private String courses;
+    private String cohort;
     private int feesPaid = 0;
     private int totalFees;
 
@@ -15,13 +14,13 @@ public class Student extends Person {
      * @param id The unique identifier of the student.
      * @param name The name of the student.
      * @param courses The courses enrolled by the student.
-     * @param cohorts The classes attended by the student.
+     * @param cohort The classes attended by the student.
      * @param feesPaid The amount of fees paid by the student.
      */
-    public Student(int id, String name, List<Course> courses, List<Cohort> cohorts, int feesPaid, int totalFees) {
+    public Student(int id, String name, String courses, String cohort, int feesPaid, int totalFees) {
         super(id, name);
         this.courses = courses;
-        this.cohorts = cohorts;
+        this.cohort = cohort;
         this.feesPaid = 0;
         this.totalFees = totalFees;
     }
@@ -29,29 +28,29 @@ public class Student extends Person {
      * Retrieves the courses enrolled by the student.
      * @return The list of courses enrolled.
      */
-    public List<Course> getCourses() {
+    public String getCourses() {
         return courses;
     }
     /**
      * Sets the courses enrolled by the student.
      * @param courses The list of courses to be set.
      */
-    public void setCourses(List<Course> courses) {
+    public void setCourses(String courses) {
         this.courses = courses;
     }
     /**
      * Retrieves the classes attended by the student.
      * @return The list of classes attended.
      */
-    public List<Cohort> getCohorts() {
-        return cohorts;
+    public String getCohort() {
+        return cohort;
     }
     /**
      * Sets the classes attended by the student.
-     * @param classes The list of classes to be set.
+     * @param cohort The list of classes to be set.
      */
-    public void setCohorts(List<Cohort> classes) {
-        this.cohorts = cohorts;
+    public void setCohort(String cohort) {
+        this.cohort = cohort;
     }
     /**
      * Retrieves the amount of fees paid by the student.
